7d98daedb48177e136a7d2a586264d0f83267842
ag617664 <95614809+ag617664@users.noreply.github.com>
2023-11-05 09:53:40 +0000

    datatypes

---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,5 @@
+"use strict" // treat all js code as newer version
+//alert(3+3) // we are using nodejs, not browser so it will not work
+console.log(3+3);
+console.log(typeof null) // null is a opbject
+console.log(typeof undefined)
